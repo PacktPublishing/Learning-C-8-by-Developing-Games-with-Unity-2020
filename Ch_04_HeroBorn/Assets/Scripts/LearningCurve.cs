988a419fd381229741c221f6384e6befbfcaef54
Harrison Ferronr <harrison@Harrisons-MacBook-Pro.local>
2020-03-02 12:27:41 -0700

    Add starting exercise files for Chapter 4

---
+++ Ch_04_HeroBorn/Assets/Scripts/LearningCurve.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LearningCurve : MonoBehaviour
+{
+    public int currentAge = 30;
+    public int addedAge = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ComputeAge();
+    }
+
+    void ComputeAge()
+    {
+        Debug.Log(currentAge + addedAge);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
+
+
