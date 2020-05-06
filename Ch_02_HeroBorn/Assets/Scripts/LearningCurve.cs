fd9ae7d7dac20bb0f9933952491f4acdc1e576c6
Harrison Ferronr <harrison@Harrisons-MacBook-Pro.local>
2020-05-05 23:59:12 -0600

    Add Ch05 project files

--- Ch_02_HeroBorn/Assets/Scripts/LearningCurve.cs
+++ Ch_02_HeroBorn/Assets/Scripts/LearningCurve.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class LearningCurve : MonoBehaviour
 {
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
